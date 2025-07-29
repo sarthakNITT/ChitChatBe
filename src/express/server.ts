5cebe04e4cfe52b75770f9f99b54d422e078a5e6
sarthakKarode <sarthakkarode@gmail.com>
2025-07-30 00:35:40 +0530

    replaced server with app in express

--- src/express/server.ts
+++ src/express/server.ts
@@ -10,7 +10,7 @@ app.use(cors())
 app.use("/", router)
 const PORT = process.env.PORT
 export async function connectExpress () {
-    app.listen(PORT, function () {
+    server.listen(PORT, function () {
         console.log("Express server running");
     })
 }
