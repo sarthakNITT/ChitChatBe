9e256ea06a9902733f74f6b3c7d4704a5f415274
sarthakKarode <sarthakkarode@gmail.com>
2025-07-29 23:53:23 +0530

    redis connect cred

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,7 +1,14 @@
 import { createClient } from "redis";
 
-export const client = createClient();
-
+export const client = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT)
+    }
+  });
+  
 client.on("error", (err): any => console.log("Redis Client Error", err));
 
 client.connect().then(() => {
